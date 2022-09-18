50d944c776b13327d0195b77b53abe437f0ac6ce
CreativeDadd <oyebolang@gmail.com>
2022-09-18 12:31:19 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * mian - print alphabets in lower case.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return 0;
+}
+
