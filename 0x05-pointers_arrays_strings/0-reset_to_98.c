21028235e55d8e64353b1e42a0386461297b8a73
CreativeDadd <oyebolang@gmail.com>
2022-09-20 15:43:18 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets the value of a variable
+ * @n: value to be reset
+ *
+ * Return: Always void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
