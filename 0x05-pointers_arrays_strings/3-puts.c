06a6800c6c68010f78d62c3b9b69b0fca80f6eb0
CreativeDadd <oyebolang@gmail.com>
2022-09-20 18:16:28 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ * @strg: pointer argument for string
+ * Return: void
+ */
+void _puts(char *strg)
+{
+	int len = 0;
+
+	while (*(strg + len) != '\0');
+	{
+		-putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}
