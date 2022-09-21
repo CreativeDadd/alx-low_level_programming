a6dd311a26ef8e9fe39df40f72523f54ebff4e4d
CreativeDadd <oyebolang@gmail.com>
2022-09-20 17:36:42 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * _strlen - renders the length of strings
+ *
+ * @s: strig's pointer
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while(*(s + len) !=0 '\0')
+		lens++;
+
+	return(length);
+}
