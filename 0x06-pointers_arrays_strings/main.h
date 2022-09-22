602db2c5e17586a499a3a6c8023f4bf84991bcdf
CreativeDadd <oyebolang@gmail.com>
2022-09-21 22:24:25 -0700

    4-rev_array

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,8 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+
+
 
 #endif
