53f2d9f0df64b09d74382f92a52dc5ba54f6dda1
CreativeDadd <oyebolang@gmail.com>
2022-09-29 12:08:26 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,17 @@
 #include "main.h"
 
+
 /**
- * print_alphabet_x10 - prints the lowercase alphabet 10 times on new lines
+ * print_alphabet - a function that prints the alphabet, in lowercase
  *
- * Return: void
+ * Return: Always 0 (Succes)
  */
-
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	char c = 'a';
-	int i, j;
-
-	for (i = 0; i < 10; i++)
+	int n;
+	for (n = 'a'; n <= 'z'; n++)
 	{
-		for (j = 0; j < 26; j++)
-		{
-			_putchar(c + j);
-		}
-		_putchar(10);
+		_putchar(n);
 	}
+	_putchar('\n');
 }
