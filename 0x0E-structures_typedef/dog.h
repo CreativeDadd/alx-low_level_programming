dbfeabef9fa151a7f67bba501ee5802404c17892
CreativeDadd <oyebolang@gmail.com>
2022-10-11 00:16:17 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - the user defined data type called dog.
+ *
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: thedog's owner
+ *
+ * Discription: the struct is used to creat the type char*, float data type.
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
