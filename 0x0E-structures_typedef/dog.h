ae6c89dc951ecdf3a2ec3e9215192cff17e483d4
CreativeDadd <oyebolang@gmail.com>
2022-10-11 01:15:33 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,8 +5,6 @@
  * @name: the dog's name
  * @age: the dog's age
  * @owner: thedog's owner
- *
- * Discription: the struct is used to creat the type char*, float data type.
  */
 struct dog{
 	char *name;
